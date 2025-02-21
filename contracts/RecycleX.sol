94b0941f78a16c31aa08a9d5d4de331ca15e5484
devkevino <38652720+devkevino@users.noreply.github.com>
2025-02-21 13:15:37 +0900

    update

--- contracts/RecycleX.sol
+++ contracts/RecycleX.sol
@@ -21,17 +21,13 @@ contract RecycleX is ERC20, ERC20Burnable, ERC20Permit, Ownable2Step {
         bytes32 r,
         bytes32 s
     ) public {
-        // 먼저 owner가 spender에게 충분한 allowance를 부여했는지 확인
         uint256 currentAllowance = allowance(owner, spender);
 
         if (currentAllowance < amount) {
             permit(owner, spender, amount, deadline, v, r, s);
         }
-
-        // permit을 호출한 후에도 allowance가 부족하면 revert
         require(allowance(owner, spender) >= amount, "ERC20: insufficient allowance");
 
-        // _spendAllowance()를 사용하여 허용된 토큰을 차감한 후 transferFrom 실행
         _spendAllowance(owner, spender, amount);
         _transfer(owner, spender, amount);
     }
